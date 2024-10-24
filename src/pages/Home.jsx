a5d81259deb5cfc11064c7be85fbe5a6ec62595a
bell-ohh <belloaise@anniecannons.com>
2024-10-24 15:40:29 -0400

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,46 +12,48 @@ import image8 from '/assets/illustration-grow-followers.webp'
 export default function Home() {
     return (
         <div>
-            <div class="first-grid">
-                <div class="third-grid">
-                    <div class="fifth-grid">
-                        <div class="seventh-grid">
-                            <p class="p1">Social media with AI <span class="gold">10X</span></p>
-                            <p class="p1"><i>Faster</i> with AI</p>
+            <div className="first-grid">
+                <div className="third-grid">
+                    <div className="fifth-grid">
+                        <div className="seventh-grid">
+                            <p className="p1">Social media with AI <span class="gold">10X</span></p>
+                            <p className="p1"><i>Faster</i> with AI</p>
                             <img src={image1} />
-                            <p class="p2">Over 4,000 5-star reiews</p>
+                            <p className="p2">Over 4,000 5-star reiews</p>
                         </div>
-                        <div class="eight-grid">
-                            <div class="ninth-grid">
+                        <div className="eight-grid">
+                            <div className="ninth-grid">
                                 <img src={image2} />                            <p>Manage multiple accounts and platforms.</p>
                             </div>
-                            <div class="tenth-grid">
+                            <div className="tenth-grid">
                                 <p>Maintain a consistence posting schedule.</p>
                                 <img src={image3} />                        </div>
                         </div>
                     </div>
-                    <div class="eleventh-grid">
-                        <p class="p3">Schedule to Social media.</p>
+                    <div className="eleventh-grid">
+                        <p className="p3">Schedule to Social media.</p>
                         <img src={image4} />
-                        <p class="p4">Optimize post timings to publish post content at the perfect time for your audience.</p>
+                        <p className="p4">Optimize post timings to publish post content at the perfect time for your audience.</p>
                     </div>
                 </div>
-                <div class="fourth-grid">
-                    <div class="twelvth-grid">
-                        <p class="p5"> {'>'} 56%</p>
-                        <p class="p6">faster audience growth</p>
-                        <img src={image5} />                </div>
-                    <div class="thirteenth-grid">
-                        <img src={image6} />                 
-                           <p>Grow followers with non-stop content.</p>
+                <div className="fourth-grid">
+                    <div className="twelvth-grid">
+                        <p className="p5"> {'>'} 56%</p>
+                        <p className="p6">faster audience growth</p>
+                        <img src={image5} />               
+                         </div>
+                    <div className="thirteenth-grid">
+                        <img src={image6} />
+                        <p>Grow followers with non-stop content.</p>
                     </div>
                 </div>
             </div>
-            <div class="second-grid">
-                <div class="header1">
-                    <p>Create and schedule content <i class="highlight">quicker.</i></p>
-                    <img src={image7} />                </div>
-                <div class="footer1">
+            <div className="second-grid">
+                <div className="header1">
+                    <p>Create and schedule content <i className="highlight">quicker.</i></p>
+                    <img src={image7} />            
+                        </div>
+                <div className="footer1">
                     <p>Write your content using AI</p>
                     <img src={image8} />
                 </div>
